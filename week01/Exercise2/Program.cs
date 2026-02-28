7d1fd0e431a377e860b1a362c8bbe2932d9988ea
ward-wolf <sward2@byupathway.edu>
2026-02-27 23:30:04 -0600

    Finished Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int percent = int.Parse(gradePercentage);
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }           
+        else if (percent >= 80)
+        {
+            letter = "B";
+        } 
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (percent >= 70)
+        {
+           Console.WriteLine($"Congratulations! You passed the class! Your grade is {letter}.");
+        }
+
+        else
+        {
+            Console.WriteLine($"Try again next time! You can improve! Sadly, your grade is {letter}.");
+        }   
     }
 }
