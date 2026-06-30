c705789036eb10bbc2ea224c88d575414719da89
ward-wolf <sward2@byupathway.edu>
2026-06-29 19:32:25 -0500

    Week 1 Exercises Initial

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,15 @@
+// Lee Ward CSE210 June 29, 2026
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
