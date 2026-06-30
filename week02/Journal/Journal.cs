fafe771503a774a9eb33a259f8e2443eb06ab5ee
ward-wolf <sward2@byupathway.edu>
2026-06-30 14:55:31 -0500

    Week 2 Journal Fix Entry Loading

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -31,7 +31,8 @@ public class Journal
         }
     }
     public void LoadFromFile(string fileName)
-    {       
+    {
+        _entries.Clear();
         string[] lines = System.IO.File.ReadAllLines(fileName);
         foreach (string line in lines)
         {
